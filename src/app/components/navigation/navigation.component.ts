52a8b66cfe9efee04c7283f3ea8d26579aa93c1e
savindu-pasintha <savindupasingtha@gmail.com>
2024-06-16 12:39:24 +0800

    varaibles value pass to HTML and access

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -8,5 +8,11 @@ import { RouterOutlet } from '@angular/router';
 })
 
 export class NavigationComponent {
- title = ""
+ val1  = "val1";
+ val2 = "va12";
+ itemList = [
+  { name: 'Item 1', description: 'This is the first item.' },
+  { name: 'Item 2', description: 'This is the second item.' },
+  { name: 'Item 3', description: 'This is the third item.' }
+];
 }
